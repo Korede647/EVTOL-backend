0986d363597c2e636171c5ecb7c515b1e67230d0
Korede Anunusor <koredeanunusor@gmai.com>
2025-01-31 22:27:48 +0100

    cloudinary!!

--- src/routes/evtol.route.ts
+++ src/routes/evtol.route.ts
@@ -1,11 +1,14 @@
 import express from "express"
 import { EvtolController } from "../controller/evtol.controller";
+import { uploadToCloudinaryMedicImage } from "../config/cloudinary.config";
 
 const evtolcontroller = new EvtolController
 const evtolRoutes = express.Router();
 
 evtolRoutes.post("/", evtolcontroller.createEvtol)
+evtolRoutes.post("/createMedic", uploadToCloudinaryMedicImage, evtolcontroller.createMedic)
 evtolRoutes.post("/loadEvtol/:S/N", evtolcontroller.loadEvtolWithMedications)
+
 evtolRoutes.get("/loadedMedication/:S/N", evtolcontroller.getLoadedMedications)
 evtolRoutes.get("/", evtolcontroller.getAvailableEvtol)
 evtolRoutes.get("/betteryLevel/:S/N", evtolcontroller.getBatteryLevel)
