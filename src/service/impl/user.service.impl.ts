8ffbf932de6a8c7c9febf8ca9a83480662fed4c7
Korede Anunusor <koredeanunusor@gmai.com>
2025-02-24 06:07:35 +0100

    user implementation

--- src/service/impl/user.service.impl.ts
+++ src/service/impl/user.service.impl.ts
@@ -1,22 +1,90 @@
 import { User } from "@prisma/client";
 import { CreateUserDTO } from "../../dto/createUser.dto";
 import { UserService } from "../user.service";
+import { db } from "../../config/db";
+import { CustomError } from "../../exceptions/customError.error";
+import { StatusCodes } from "http-status-codes";
 
 export class UserServiceImpl implements UserService{
-    createUser(data: CreateUserDTO): Promise<User> {
-        throw new Error("Method not implemented.");
+    async createUser(data: CreateUserDTO): Promise<User> {
+         const isUserExists = await db.user.findFirst({
+            where: {
+                email: data.email
+            },
+         })
+         if(isUserExists){
+            throw new CustomError(
+                StatusCodes.CONFLICT,
+                "Oops a user with this id already exists."
+            )
+         }
+
+         const user = await db.user.create({
+            data: {
+                firstName: data.firstName,
+                lastName: data.lastName,
+                email: data.email,
+                password: data.password
+            }
+         })
+         return user
     }
-    getUserById(id: number): Promise<User | null> {
-        throw new Error("Method not implemented.");
+
+    async getUserById(id: number): Promise<User | null> {
+        const user = await db.user.findUnique({
+            where: {
+                id: id
+            },
+        })
+        if(!user){
+            throw new CustomError(
+                StatusCodes.NOT_FOUND,
+                "No user with id specified"
+            )
+        }
+        return user
     }
-    getAllUsers(): Promise<User[]> {
-        throw new Error("Method not implemented.");
+    
+    async getAllUsers(): Promise<User[]> {
+        return await db.user.findMany()
     }
-    updateUser(id: number, data: Partial<CreateUserDTO>): Promise<User> {
-        throw new Error("Method not implemented.");
+
+    async updateUser(id: number, data: Partial<CreateUserDTO>): Promise<User> {
+       const isUserExists = await db.user.findFirst({
+        where: {
+            id
+        },
+       })
+       if(!isUserExists){
+        throw new CustomError(StatusCodes.CONFLICT, "User does not exist")
+       }
+       const user = await db.user.update({
+        where: {
+            id: id
+        },
+        data
+       })
+
+       return user
     }
-    deleteUser(id: number): Promise<void> {
-        throw new Error("Method not implemented.");
+
+    async deleteUser(id: number): Promise<void> {
+       const isUserExists = await db.user.findUnique({
+        where: {
+            id
+        },
+       })
+       if(!isUserExists){
+        throw new CustomError(
+            StatusCodes.NOT_FOUND,
+            "User not found"
+        )
+       }
+       await db.user.delete({
+        where: {
+            id
+        }
+       })
     }
     
 }
